ff862884e1ee89f46bda2e1d0b662be32a363448
shohei.okugi <shohei.okugi@ghelia.com>
2024-10-05 14:12:27 +0900

    Updates

--- app/immutable/chunks/navigation.client.CuBaxDXV.js
+++
@@ -1,1 +0,0 @@
-import{g as o}from"./entry.B4P31jY6.js";import{b as r}from"./paths.ipqIoKhU.js";function s(s){o(`${r}${s}`)}export{s as n};
--- app/immutable/entry/start.Cxzv4HIe.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/entry.B4P31jY6.js";export{r as start};
